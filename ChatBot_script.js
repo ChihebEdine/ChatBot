e7c84c564cea9ac79bc7bbca9023956ad07e252a
Chiheb Eddine NAJJAR <38221399+ChihebEdine@users.noreply.github.com>
2019-04-20 23:37:46 +0200

    Update ChatBot_script.js

--- ChatBot_script.js
+++ ChatBot_script.js
@@ -7,6 +7,7 @@ document.querySelector('button').addEventListener('click',function clickHandler(
     User_message.className = "User_Message";
     User_message.innerHTML = Input.value;
     MC.appendChild(User_message);
+    MC.scrollTop = MC.scrollHeight;
     Input.value = "";
 
     e.preventDefault();
@@ -21,6 +22,8 @@ document.querySelector('button').addEventListener('click',function clickHandler(
         Bot_message.className = "Bot_Message";
         Bot_message.innerHTML = "Sorry ! I am not available for the moment";  //Get the response of the API here ...
         MC.appendChild(Bot_message);
+        MC.scrollTop = MC.scrollHeight;
     },1000);  // loading time ...
+    
 
 },false);
