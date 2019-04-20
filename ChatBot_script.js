5154d23e7446765facd879546ccb5edfa9d58628
Chiheb Eddine NAJJAR <38221399+ChihebEdine@users.noreply.github.com>
2019-04-20 20:22:33 +0200

    Add files via upload

---
+++ ChatBot_script.js
@@ -0,0 +1,26 @@
+
+document.querySelector('button').addEventListener('click',function clickHandler(e){
+
+    let MC = document.getElementById('MC');
+    let Input = document.getElementById('Input');
+    let User_message = document.createElement('div');
+    User_message.className = "User_Message";
+    User_message.innerHTML = Input.value;
+    MC.appendChild(User_message);
+    Input.value = "";
+
+    e.preventDefault();
+    var self = this;
+    setTimeout(function(){
+        self.className = 'loading';
+    },100);
+
+    setTimeout(function(){
+        self.className = 'ready';
+        let Bot_message = document.createElement('div');
+        Bot_message.className = "Bot_Message";
+        Bot_message.innerHTML = "Sorry ! I am not available for the moment";  //Get the response of the API here ...
+        MC.appendChild(Bot_message);
+    },1000);  // loading time ...
+
+},false);
