5e07f1f2140d12c1bbdd69d09ca2c2ecbf459c94
Chiheb Eddine NAJJAR <chihebnajjar22@gmail.com>
2019-05-28 21:09:21 +0200

    cleaning

--- chatbot-frontend/src/index.js
+++ chatbot-frontend/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import ChatBot from './ChatBot';
 import * as serviceWorker from './serviceWorker';
-import BotMessage from './BotMessage';
+
 
 ReactDOM.render(<ChatBot />, document.getElementById('root'));
-///ReactDOM.render(<BotMessage content = 'hello' />, document.getElementById('id')); à regarder !!
+
 
 
 // If you want your app to work offline and load faster, you can change
